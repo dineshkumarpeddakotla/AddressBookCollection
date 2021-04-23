6ae0389a2b31abd6beb327bd899a57303c128b6c
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-23 15:33:06 +0530

    ADDED: countContactDetailsByCityOrState Method to Count Persons by City or State

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -8,7 +8,6 @@ public class AddressBookMain {
     private final HashMap<String, List<List<ContactDetails>>> cityContactDetailsDic = new HashMap<>();
     private final HashMap<String, List<List<ContactDetails>>> stateContactDetailsDic = new HashMap<>();
 
-
     static AddressBookMain addressBookMain = new AddressBookMain();
 
     public static void main(String[] args) {
@@ -16,7 +15,7 @@ public class AddressBookMain {
         boolean isExit = false;
         while (!isExit) {
             System.out.println("1.Create New Address Book\n2.Add Person to AddressBook " +
-                               "\n3.View Person by City\n4.View Person by State\n5.Exit");
+                               "\n3.View Person by City\n4.View Person by State\n5.Persons Count in City or State\n6.Exit");
             Scanner scanner = new Scanner(System.in);
             int choice = scanner.nextInt();
             System.out.println(choice);
@@ -50,16 +49,21 @@ public class AddressBookMain {
                     String state = search.nextLine();
                     addressBookMain.viewPersonByState(state);
                     break;
+                case 5:
+                    System.out.println("Enter City or State to Get Count");
+                    Scanner countInput = new Scanner(System.in);
+                    String cityOrState = countInput.nextLine();
+                    addressBookMain.countContactDetailsByCityOrState(cityOrState);
+                    break;
                 default:
                     isExit = true;
                     break;
             }
         }
-        addressBookMain.addressBookDic.forEach((key, value) ->
-                                               System.out.println("Key : "+key + " Value-> " + value));
+        addressBookMain.addressBookDic.forEach((key, value) -> System.out.println("Key : "+key + " Value-> " + value));
     }
 
-    public void viewPersonByCity(String city) {
+    private void viewPersonByCity(String city) {
         List<List<ContactDetails>> cityContactDetailsList = new ArrayList<>();
         for (Map.Entry<String,AddressBook> addressBookEntry : addressBookMain.addressBookDic.entrySet()) {
             List<ContactDetails> cityData = addressBookEntry.getValue()
@@ -73,8 +77,8 @@ public class AddressBookMain {
         System.out.println(cityContactDetailsDic.toString());
     }
 
-    public void viewPersonByState(String state) {
-        List<List<ContactDetails>> stateContactDetailsList = new ArrayList<>();
+    private void viewPersonByState(String state) {
+        List<List<ContactDetails>>stateContactDetailsList = new ArrayList<>();
         for (Map.Entry<String,AddressBook> addressBookEntry : addressBookMain.addressBookDic.entrySet()) {
             List<ContactDetails> stateData = addressBookEntry.getValue()
                                                              .getPersonsData().stream()
@@ -84,5 +88,18 @@ public class AddressBookMain {
         }
         stateContactDetailsDic.put(state, stateContactDetailsList);
         System.out.println(stateContactDetailsDic.toString());
+    }
+
+    private void countContactDetailsByCityOrState(String cityOrState) {
+            int count = 0;
+            for (Map.Entry<String, AddressBook> addressBookEntry : addressBookDic.entrySet()) {
+                for (int i = 0; i < (addressBookEntry.getValue()).getPersonsData().size(); i++) {
+                    ContactDetails contactDetails = addressBookEntry.getValue().getPersonsData().get(i);
+                    if (contactDetails.getCity().equals(cityOrState)||contactDetails.getState().equals(cityOrState)) {
+                        count++;
+                    }
+                }
+            }
+            System.out.println("Total Number of ContactDetails in  " +cityOrState + " is: " + count);
     }
 }
